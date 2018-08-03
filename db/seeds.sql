fb07711e7cb451c017101569db6de82047a35f85
ZCOLT <colton.zac@gmail.com>
2018-08-02 18:54:22 -0600

    first commit

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name) VALUES ('Bacon Burger');
+INSERT INTO burgers (burger_name) VALUES ('Texas Burger');
+INSERT INTO burgers (burger_name) VALUES ('Cheese Buger');
+
