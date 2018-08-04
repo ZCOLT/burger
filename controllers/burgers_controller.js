093e2a1cc8f9faa30a830c6039a2621851a88128
ZCOLT <colton.zac@gmail.com>
2018-08-04 15:55:17 -0600

    backend created

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -35,6 +35,7 @@ router.put("/api/burgers/:id", function(req, res) {
   burger.updateOne({
     devoured: req.body.devoured
   }, condition, function(result) {
+    console.log(result)
     if (result.changedRows == 0) {
       // If no rows were changed, then the ID must not exist, so 404
       return res.status(404).end();
